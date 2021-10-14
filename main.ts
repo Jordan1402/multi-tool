1d1002f76e01e82da26dd50fd40738a40e7ebdfd
Jordan1402 <92406005+Jordan1402@users.noreply.github.com>
2021-10-14 20:13:19 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,256 +1,39 @@
-function compass_heading () {
-    if (input.compassHeading() > 0 && input.compassHeading() < 10) {
-        basic.showNumber(10)
-    }
-    if (input.compassHeading() > 10 && input.compassHeading() < 20) {
-        basic.showNumber(20)
-    }
-    if (input.compassHeading() > 20 && input.compassHeading() < 30) {
-        basic.showNumber(30)
-    }
-    if (input.compassHeading() > 30 && input.compassHeading() < 40) {
-        basic.showNumber(40)
-    }
-    if (input.compassHeading() > 40 && input.compassHeading() < 50) {
-        basic.showNumber(50)
-    }
-    if (input.compassHeading() > 50 && input.compassHeading() < 60) {
-        basic.showNumber(60)
-    }
-    if (input.compassHeading() > 60 && input.compassHeading() < 70) {
-        basic.showNumber(70)
-    }
-    if (input.compassHeading() > 70 && input.compassHeading() < 80) {
-        basic.showNumber(80)
-    }
-    if (input.compassHeading() > 80 && input.compassHeading() < 90) {
-        basic.showNumber(90)
-    }
-    if (input.compassHeading() > 90 && input.compassHeading() < 100) {
-        basic.showNumber(100)
-    }
-    if (input.compassHeading() > 100 && input.compassHeading() < 110) {
-        basic.showNumber(110)
-    }
-    if (input.compassHeading() > 110 && input.compassHeading() < 120) {
-        basic.showNumber(120)
-    }
-    if (input.compassHeading() > 120 && input.compassHeading() < 130) {
-        basic.showNumber(130)
-    }
-    if (input.compassHeading() > 130 && input.compassHeading() < 140) {
-        basic.showNumber(140)
-    }
-    if (input.compassHeading() > 140 && input.compassHeading() < 150) {
-        basic.showNumber(150)
-    }
-    if (input.compassHeading() > 150 && input.compassHeading() < 160) {
-        basic.showNumber(160)
-    }
-    if (input.compassHeading() > 160 && input.compassHeading() < 170) {
-        basic.showNumber(170)
-    }
-    if (input.compassHeading() > 170 && input.compassHeading() < 180) {
-        basic.showNumber(180)
-    }
-    if (input.compassHeading() > 180 && input.compassHeading() < 190) {
-        basic.showNumber(190)
-    }
-    if (input.compassHeading() > 190 && input.compassHeading() < 200) {
-        basic.showNumber(200)
-    }
-    if (input.compassHeading() > 200 && input.compassHeading() < 210) {
-        basic.showNumber(210)
-    }
-    if (input.compassHeading() > 210 && input.compassHeading() < 220) {
-        basic.showNumber(220)
-    }
-    if (input.compassHeading() > 220 && input.compassHeading() < 230) {
-        basic.showNumber(230)
-    }
-    if (input.compassHeading() > 230 && input.compassHeading() < 240) {
-        basic.showNumber(240)
-    }
-    if (input.compassHeading() > 240 && input.compassHeading() < 250) {
-        basic.showNumber(250)
-    }
-    if (input.compassHeading() > 250 && input.compassHeading() < 260) {
-        basic.showNumber(260)
-    }
-    if (input.compassHeading() > 260 && input.compassHeading() < 270) {
-        basic.showNumber(270)
-    }
-    if (input.compassHeading() > 270 && input.compassHeading() < 280) {
-        basic.showNumber(280)
-    }
-    if (input.compassHeading() > 280 && input.compassHeading() < 290) {
-        basic.showNumber(290)
-    }
-    if (input.compassHeading() > 290 && input.compassHeading() < 300) {
-        basic.showNumber(300)
-    }
-    if (input.compassHeading() > 300 && input.compassHeading() < 310) {
-        basic.showNumber(310)
-    }
-    if (input.compassHeading() <= 310 && input.compassHeading() < 320) {
-        basic.showNumber(320)
-    }
-    if (input.compassHeading() > 320 && input.compassHeading() < 330) {
-        basic.showNumber(330)
-    }
-    if (input.compassHeading() > 330 && input.compassHeading() < 340) {
-        basic.showNumber(340)
-    }
-    if (input.compassHeading() > 340 && input.compassHeading() < 350) {
-        basic.showNumber(350)
-    }
-    if (input.compassHeading() > 350 && input.compassHeading() < 360) {
-        basic.showNumber(360)
-    }
-}
+let sos = 0
 input.onButtonPressed(Button.A, function () {
-    read_temp()
+    basic.showNumber(input.temperature())
 })
 input.onButtonPressed(Button.B, function () {
-    compass_heading()
+    basic.showNumber(input.compassHeading())
 })
-function read_temp () {
-    if (input.temperature() == -15) {
-        basic.showNumber(-15)
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    sos = -1
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
+        sos = 1
+        while (sos >= 0) {
+            for (let index = 0; index < 3; index++) {
+                pins.digitalWritePin(DigitalPin.P0, 1)
+                basic.pause(500)
+                pins.digitalWritePin(DigitalPin.P0, 0)
+                basic.pause(500)
+            }
+            for (let index = 0; index < 3; index++) {
+                pins.digitalWritePin(DigitalPin.P0, 1)
+                basic.pause(1000)
+                pins.digitalWritePin(DigitalPin.P0, 0)
+                basic.pause(1000)
+            }
+            for (let index = 0; index < 3; index++) {
+                pins.digitalWritePin(DigitalPin.P0, 1)
+                basic.pause(500)
+                pins.digitalWritePin(DigitalPin.P0, 0)
+                basic.pause(500)
+            }
+            basic.pause(2000)
+        }
     }
-    if (input.temperature() == -14) {
-        basic.showNumber(-14)
-    }
-    if (input.temperature() == -13) {
-        basic.showNumber(-13)
-    }
-    if (input.temperature() == -12) {
-        basic.showNumber(-12)
-    }
-    if (input.temperature() == -11) {
-        basic.showNumber(-11)
-    }
-    if (input.temperature() == -10) {
-        basic.showNumber(-10)
-    }
-    if (input.temperature() == -9) {
-        basic.showNumber(-9)
-    }
-    if (input.temperature() == -8) {
-        basic.showNumber(-8)
-    }
-    if (input.temperature() == -7) {
-        basic.showNumber(-7)
-    }
-    if (input.temperature() == -6) {
-        basic.showNumber(-6)
-    }
-    if (input.temperature() == -5) {
-        basic.showNumber(-5)
-    }
-    if (input.temperature() == -4) {
-        basic.showNumber(-4)
-    }
-    if (input.temperature() == -3) {
-        basic.showNumber(-3)
-    }
-    if (input.temperature() == -2) {
-        basic.showNumber(-2)
-    }
-    if (input.temperature() == -1) {
-        basic.showNumber(-1)
-    }
-    if (input.temperature() == 0) {
-        basic.showNumber(0)
-    }
-    if (input.temperature() == 1) {
-        basic.showNumber(1)
-    }
-    if (input.temperature() == 2) {
-        basic.showNumber(2)
-    }
-    if (input.temperature() == 3) {
-        basic.showNumber(3)
-    }
-    if (input.temperature() == 4) {
-        basic.showNumber(4)
-    }
-    if (input.temperature() == 5) {
-        basic.showNumber(5)
-    }
-    if (input.temperature() == 6) {
-        basic.showNumber(6)
-    }
-    if (input.temperature() == 7) {
-        basic.showNumber(7)
-    }
-    if (input.temperature() == 8) {
-        basic.showNumber(8)
-    }
-    if (input.temperature() == 9) {
-        basic.showNumber(9)
-    }
-    if (input.temperature() == 10) {
-        basic.showNumber(11)
-    }
-    if (input.temperature() == 12) {
-        basic.showNumber(12)
-    }
-    if (input.temperature() == 13) {
-        basic.showNumber(13)
-    }
-    if (input.temperature() == 14) {
-        basic.showNumber(14)
-    }
-    if (input.temperature() == 15) {
-        basic.showNumber(15)
-    }
-    if (input.temperature() == 16) {
-        basic.showNumber(16)
-    }
-    if (input.temperature() == 17) {
-        basic.showNumber(17)
-    }
-    if (input.temperature() == 18) {
-        basic.showNumber(18)
-    }
-    if (input.temperature() == 19) {
-        basic.showNumber(19)
-    }
-    if (input.temperature() == 20) {
-        basic.showNumber(20)
-    }
-    if (input.temperature() == 21) {
-        basic.showNumber(21)
-    }
-    if (input.temperature() == 22) {
-        basic.showNumber(22)
-    }
-    if (input.temperature() == 23) {
-        basic.showNumber(23)
-    }
-    if (input.temperature() == 24) {
-        basic.showNumber(24)
-    }
-    if (input.temperature() == 25) {
-        basic.showNumber(25)
-    }
-    if (input.temperature() == 26) {
-        basic.showNumber(26)
-    }
-    if (input.temperature() == 27) {
-        basic.showNumber(27)
-    }
-    if (input.temperature() == 28) {
-        basic.showNumber(28)
-    }
-    if (input.temperature() == 29) {
-        basic.showNumber(29)
-    }
-    if (input.temperature() == 30) {
-        basic.showNumber(30)
-    }
-}
+})
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B)) {
         basic.clearScreen()
